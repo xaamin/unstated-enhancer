a2991d4f2db588f8645448a82019d4e3eec2c43e
Benjamín Martínez Mateos <xaamin@outlook.com>
2018-11-13 07:25:06 -0600

    Added support to map state and containers to props

--- src/unstated-connect.tsx
+++ src/unstated-connect.tsx
@@ -5,30 +5,62 @@ const isObject = (value) => {
   return value && typeof value === 'object' && value.constructor === Object;
 }
 
-const connect = ( options: any = {}, config: any = {} ) => {
-  if (!isObject(options)) {
-    throw new Error('Connect needs an object for containers')
+const makeContainers = (containers: any, config: any): any => {
+  let keys: string[] = Object.keys(config);
+  let inject = {};
+
+  for (let index in keys) {
+    inject[keys[index]] = containers[index];
   }
 
-  if (config) {
-    // Do awesome things over containers
-    // with the config
+  return inject;
+}
+
+const connect = ( options: any = {}, mapStateToProps?: (state: any) => any, mapContainersToProps?: (containers: any) => any) => {
+  if (!isObject(options)) {
+    throw new Error('Connect needs an object with containers')
   }
 
   let _containers: any = Object.values(options);;
-  let keys: string[] = Object.keys(options);
+
+  let inject: any;
+  let mappedContainers: any;
 
   return Component => props => {
+    let ConnectedComponenet = Component;
+
     return (
       <Subscribe to={ _containers }>
         { (...containers) => {
-          let inject: any = {};
 
-          for (let index in keys) {
-            inject[keys[index]] = containers[index];
+          if (!inject) {
+            inject = makeContainers(containers, options);
           }
 
-          return <Component {...props} containers={ inject } />;
+          if (!mappedContainers && mapContainersToProps) {
+            mappedContainers = mapContainersToProps(inject);
+          }
+
+          let newProps = {
+            ...props
+          }
+
+          if (!mapContainersToProps) {
+            newProps.containers = inject;
+          } else {
+            newProps.containers = mappedContainers;
+          }
+
+          if (mapStateToProps) {
+            let mappedState: any = mapStateToProps(inject);
+
+            newProps = {
+              ...mappedState,
+              ...newProps
+            }
+          }
+
+          return <ConnectedComponenet { ...newProps } />;
         } }
       </Subscribe>
     )
